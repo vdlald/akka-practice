9e3c4d1319ad3fe6d8990b3b4f000e3f80811de0
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-07-09 07:51:55 +0300

    clean

--- beard/src/main/java/com/practice/useakka/Main.java
+++ beard/src/main/java/com/practice/useakka/Main.java
@@ -1,6 +1,5 @@
 package com.practice.useakka;
 
-import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.Terminated;
@@ -9,7 +8,6 @@ import akka.actor.typed.javadsl.Behaviors;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.function.IntSupplier;
 
 public class Main {
@@ -22,15 +20,6 @@ public class Main {
                     final ScheduledExecutorService executorService = Executors
                             .newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
                     final ThreadLocalRandom random = ThreadLocalRandom.current();
-
-                    final ActorRef<BarberShop.Command> barberShop = context
-                            .spawn(BarberShop.create(waitingRoomSize), "BarberShop");
-
-                    final IntSupplier clientNumberGenerator = createIntGenerator();
-                    executorService.scheduleAtFixedRate(() -> {
-                        final Client client = new Client("Client " + clientNumberGenerator.getAsInt());
-                        barberShop.tell(new BarberShop.ReceiveClient(client));
-                    }, 0, 300 + random.nextInt(50, 100), TimeUnit.MILLISECONDS);
 
                     return Behaviors.receive(Void.class)
                             .onSignal(Terminated.class, sig -> Behaviors.stopped())
